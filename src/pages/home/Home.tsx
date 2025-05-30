86c50d14ec162446bd9b85b3f1e6b99fe48eaa50
Jose-Q91 <josecito.qs91@gmail.com>
2025-05-30 10:06:49 -0300

    Se creo archivo db.json y se instalo json-server, para una api que se consumira desde el componente Home.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react"
 import { Hero } from "../../components/ui/Hero/Hero"
 
 export const Home = () => {
+
+  const [products, setProducts] = useState([])
+  const getProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/products')
+      const data = await response.json()
+      setProducts(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+  console.log(products)
+
   return (
     <>
       <Hero />
